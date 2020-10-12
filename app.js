973c76a90ec2563b9e3f562bd77a9be1b598dc93
Danish <danishbalti1@gmail.com>
2020-10-12 15:57:48 +0500

    d

--- app.js
+++ app.js
@@ -11,94 +11,94 @@ const data = new ProjectData();
 
 data.add(
   "https://danishbalti1.github.io/Chef/",
-  "Project_Images/dani.png",
+  "Project_Images/dani.jpg",
   "Dani Chef"
 );
 
 data.add(
   "https://danishbalti1.github.io/DeFlex/",
-  "Project_Images/netflix.png",
+  "Project_Images/netflix.jpg",
   "Netflix Clone"
 );
 
 data.add(
   "https://danishbalti1.github.io/Our-Portfolio/",
-  "Project_Images/our-portfolio.png",
+  "Project_Images/our-portfolio.jpg",
   "My Team Portfolio"
 );
 
 data.add(
   "https://flamboyant-panini-649821.netlify.app/",
-  "Project_Images/news.png",
+  "Project_Images/news.jpg",
   "News Grid Article"
 );
 
 data.add(
   "https://upbeat-montalcini-73ca69.netlify.app/index.html",
-  "Project_Images/feel-the-music.png",
+  "Project_Images/feel-the-music.jpg",
   "Feel The Music"
 );
 data.add(
   "https://danishbalti1.github.io/Responsive-Website/",
-  "Project_Images/microsoft.png",
+  "Project_Images/microsoft.jpg",
   "MircorSoft Clone"
 );
 
 data.add(
   "https://danishbalti1.github.io/EdgeLedger-/",
-  "Project_Images/edgeledger.png",
+  "Project_Images/edgeledger.jpg",
   "EdgeLedger"
 );
 
 data.add(
   "https://danishbalti1.github.io/Hotel/",
-  "Project_Images/hotel.png",
+  "Project_Images/hotel.jpg",
   "HTB Hotel"
 );
 data.add(
   "https://danishbalti1.github.io/GymSo/",
-  "Project_Images/gymso.png",
+  "Project_Images/gymso.jpg",
   "Gymso Gym"
 );
 data.add(
   "https://danishbalti1.github.io/Portfolio/",
-  "Project_Images/portfolio.png",
+  "Project_Images/portfolio.jpg",
   "Portfolio"
 );
 data.add(
   "https://danishbalti1.github.io/HeadPhone-Blog/",
-  "Project_Images/blog.png",
+  "Project_Images/blog.jpg",
   "HeadPhone Blog"
 );
 data.add(
   "https://danishbalti1.github.io/Khan-Academy/",
-  "Project_Images/khan.png",
+  "Project_Images/khan.jpg",
   "Khan Academy"
 );
 data.add(
   "https://danishbalti1.github.io/Testing/",
-  "Project_Images/namzo.png",
+  "Project_Images/namzo.jpg",
   "Namzo"
 );
 data.add(
   "https://danishbalti1.github.io/CSS-Grid-Test/",
-  "Project_Images/grid.png",
+  "Project_Images/grid.jpg",
   "NianZo"
 );
 
 data.add(
   "https://danishbalti1.github.io/Dice-Game/",
-  "Project_Images/dice-game.png",
+  "Project_Images/dice-game.jpg",
   "Dice Game"
 );
 data.add(
   "https://danishbalti1.github.io/UniPorject/",
-  "Project_Images/uni-project.png",
+  "Project_Images/uni-project.jpg",
   "First University Project"
 );
 data.add(
   "https://danishbalti1.github.io/Template-/",
-  "Project_Images/template-clone.png",
+  "Project_Images/template-clone.jpg",
   "SeyanZo"
 );
 
